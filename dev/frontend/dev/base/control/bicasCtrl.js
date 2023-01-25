186dc3a136a5770b5c6a7f00df554590514549a2
bznice <lfm.videira@gmail.com>
2023-01-25 06:29:25 +0000

    TODO-0001 - almost done, just saving

--- dev/frontend/dev/base/control/bicasCtrl.js
+++ dev/frontend/dev/base/control/bicasCtrl.js
@@ -2,8 +2,47 @@ angular.module("bicas", []);
 angular.module("bicas").controller("bicasCtrl", ['$scope', function ($scope) {
 
     //- DATA -----------------------------------------------------------------------------------------------------------
+    
+    $scope.partialSelectorInHall = true;
+    $scope.partialSelectorInLobby = false;
+    $scope.partialSelectorInLounge = false;
 
+    $scope.player = null;
     //- FUNCTIONS ------------------------------------------------------------------------------------------------------
 
+    // PARTIAL CONTROL METHODS
+    $scope.goToHall = function () {
+        $scope.partialSelectorInHall = true;
+        $scope.partialSelectorInLobby = false;
+        $scope.partialSelectorInLounge = false;
+    }
+
+    $scope.goToLobby = function () {
+        $scope.partialSelectorInHall = false;
+        $scope.partialSelectorInLobby = true;
+        $scope.partialSelectorInLounge = false;
+    }
+
+    $scope.goToLounge = function () {
+        $scope.partialSelectorInHall = false;
+        $scope.partialSelectorInLobby = false;
+        $scope.partialSelectorInLounge = true;
+    }
+
+    // HALL CONTROL METHODS
+    $scope.hallStageCreatePlayer = function (playerParam) {
+        $scope.player = $scope.hallStageConfigPlayerNotInSession(angular.copy(playerParam));
+        delete playerParam;
+        $scope.goToLobby();
+    }
+
+    $scope.hallStageConfigPlayerNotInSession = function (playerParam) {
+        return { nick: playerParam.nick.toLowerCase(), sessionCode: "" };
+    }
+
+    // LOBBY CONTROL METHODS
+
+    // LOUNGE CONTROL METHODS
+    
     //------------------------------------------------------------------------------------------------------------------
-}
+}]);
